bc244750a0112f9ebdd87e32d3a6e9cea0a358aa
Yu-Hsin Shih <yuhsinshih@gmail.com>
2014-07-11 16:34:00 -0700

    Add Splash screen

--- src/com/example/flickassist/MainActivity.java
+++ src/com/example/flickassist/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.flickassist;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+ 
 public class MainActivity extends Activity {
-
+ 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+ 
     }
 }
---
+++ src/com/example/flickassist/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.example.flickassist;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SplashActivity extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 5000;
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+ 
+        new Handler().postDelayed(new Runnable() {
+ 
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+ 
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+ 
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
