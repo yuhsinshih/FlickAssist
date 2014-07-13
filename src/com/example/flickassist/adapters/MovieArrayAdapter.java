13089e065cc35e2a7feecec64c62001d6ee1b68d
Yu-Hsin Shih <yuhsinshih@gmail.com>
2014-07-13 16:43:36 -0700

    Add adapter and movie_item layout

---
+++ src/com/example/flickassist/adapters/MovieArrayAdapter.java
@@ -0,0 +1,47 @@
+package com.example.flickassist.adapters;
+
+import java.util.List;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.flickassist.R;
+import com.example.flickassist.models.Movie;
+import com.nostra13.universalimageloader.core.ImageLoader;
+
+public class MovieArrayAdapter extends ArrayAdapter<Movie>{
+
+	public MovieArrayAdapter(Context context, int resource,
+			int textViewResourceId, List<Movie> objects) {
+		super(context, resource, textViewResourceId, objects);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// Get the data item for position
+
+		// Find or inflate the template
+		View v;	// for performance reason
+		if (convertView == null) {
+			LayoutInflater inflator = LayoutInflater.from(getContext());
+//			v = inflator.inflate(R.layout. , parent, false);
+			v = inflator.inflate(R.layout.movie_item, parent, false);
+		} else {
+			v = convertView;
+		}
+		// Find the views within template
+
+		// Populate views with movie data
+
+		return v;
+	}
+
+}
